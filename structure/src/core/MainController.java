6b27af3a7ac3b66481b63f94352ca45ec9afb5c9
Andre Matutat <amatutat@web.de>
2021-10-12 14:35:52 +0200

    add basic loop and own implementation interface

--- structure/src/core/MainController.java
+++ structure/src/core/MainController.java
@@ -8,11 +8,30 @@ public class MainController {
 
     private final ArrayList<IEntity> dungeonEntities = new ArrayList<>();
 
-    public void update() {
+    private void updateEntities() {
         dungeonEntities.removeIf(obj -> obj.deleteable());
         dungeonEntities.forEach(obj -> obj.update());
     }
 
+    public void loop(){
+        //firstFrame
+        beginFrame();
+        updateEntities();
+        //updateLevel
+        //updateGrafik
+        //update Camera
+        //update GameBatch
+        //update hud
+        endFrame();
+    }
 
+    // --------------------------- OWN IMPLEMENTATION ---------------------------
+    protected void setup() {}
 
+    protected void beginFrame() {}
+
+    protected void endFrame() {}
+
+    public void onLevelLoad() {}
+    // --------------------------- END OWN IMPLEMENTATION ------------------------
 }
