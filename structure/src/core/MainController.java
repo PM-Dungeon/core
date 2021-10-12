89cbce462f2a11f851b26ab047f7dfcd339aef74
Andre Matutat <amatutat@web.de>
2021-10-12 14:32:37 +0200

    add entitycontroll

--- structure/src/core/MainController.java
+++ structure/src/core/MainController.java
@@ -1,4 +1,18 @@
 package core;
 
+import core.interfaces.IEntity;
+
+import java.util.ArrayList;
+
 public class MainController {
+
+    private final ArrayList<IEntity> dungeonEntities = new ArrayList<>();
+
+    public void update() {
+        dungeonEntities.removeIf(obj -> obj.deleteable());
+        dungeonEntities.forEach(obj -> obj.update());
+    }
+
+
+
 }
