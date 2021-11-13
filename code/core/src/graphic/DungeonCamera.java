4f82c62d477bd76731ce90ae15934bda93f78a19
Tobias Grothe <85501570+Tobias321@users.noreply.github.com>
2021-11-13 19:00:16 +0100

    Add SpotBugs to Gradle

--- code/core/src/graphic/DungeonCamera.java
+++ code/core/src/graphic/DungeonCamera.java
@@ -1,6 +1,9 @@
 package graphic;
 
 public class DungeonCamera {
+
+    private String aUnusedNonFinalVariable = aMethod();
+
     static String aMethod() {
         // a provocated violation
         int i = 42;
