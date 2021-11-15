0d8eac83337ca117e87fcd5f45c29628185bc874
Andre Matutat <amatutat@web.de>
2021-11-15 14:38:02 +0100

    TextureFactory

--- code/core/src/graphic/TextureFactory.java
+++ code/core/src/graphic/TextureFactory.java
@@ -1,4 +1,13 @@
 package graphic;
 
+import com.badlogic.gdx.graphics.Texture;
+
+/**
+ * Factorypattern for Textures so we can mock it
+ */
 public class TextureFactory {
+
+    public Texture getTexture(String path){
+        return new Texture(path);
+    }
 }
