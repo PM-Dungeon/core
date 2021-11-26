ccdb3580517a22bd8f4eb36a080492e45667faaf
Tobias Grothe <85501570+Tobias321@users.noreply.github.com>
2021-11-26 09:27:37 +0100

    Update MainController.java Set the camera

--- code/core/src/interfaces/IDrawable.java
+++ code/core/src/interfaces/IDrawable.java
@@ -3,7 +3,6 @@ package interfaces;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import controller.GraphicController;
-import graphic.TextureFactory;
 import tools.Point;
 /** Should be implement by all objects that are drawable but have no animation */
 public interface IDrawable {
