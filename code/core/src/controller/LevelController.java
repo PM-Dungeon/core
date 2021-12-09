d42351d9c629e16a21b1f428a8d7866221f6f967
Tobias Grothe <85501570+Tobias321@users.noreply.github.com>
2021-12-09 07:39:38 +0100

    refactoring, LevelController...

--- code/core/src/controller/LevelController.java
+++ code/core/src/controller/LevelController.java
@@ -5,5 +5,5 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class LevelController {
     public LevelController(SpriteBatch batch, GraphicController graphicController) {}
 
-    protected void update() {}
+    public void update() {}
 }
