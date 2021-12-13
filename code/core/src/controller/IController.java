47bb9b7248ac4cf5c343b0623248c85802c7740b
Tobias Grothe <85501570+Tobias321@users.noreply.github.com>
2021-12-13 17:41:25 +0100

    interface modifiers

--- code/core/src/controller/IController.java
+++ code/core/src/controller/IController.java
@@ -6,17 +6,17 @@ import java.util.Set;
 public interface IController<T> {
 
     /** Updates all Elements that are registered at this controller */
-    public void update();
+    void update();
     /** Register an element. */
-    public void add(T element);
+    void add(T element);
     /** Removes an element from the set. */
-    public void remove(T element);
+    void remove(T element);
     /** Removes all elements from the set. */
-    public void removeAll();
+    void removeAll();
     /** Returns <code>true</code> if the element is registered. */
-    public boolean contains(T element);
+    boolean contains(T element);
     /** Returns a copy set of all elements. */
-    public Set<T> getSet();
+    Set<T> getSet();
     /** Returns a copy list of all elements. */
-    public List<T> getList();
+    List<T> getList();
 }
