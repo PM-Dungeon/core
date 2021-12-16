0a87ea1071d0aa068b1771250395cc8dbb29290e
Tobias Grothe <85501570+Tobias321@users.noreply.github.com>
2021-12-16 18:19:41 +0100

    DungeonCameraTest.java

--- code/core/test/graphic/DungeonCameraTest.java
+++ code/core/test/graphic/DungeonCameraTest.java
@@ -1,28 +1,92 @@
 package graphic;
 
-import org.junit.Test;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Frustum;
+import com.badlogic.gdx.math.Vector3;
+import interfaces.IEntity;
+import junit.framework.TestCase;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.api.support.membermodification.MemberMatcher;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
+import tools.Point;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyFloat;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(DungeonCamera.class)
-public class DungeonCameraTest {
-    @Test
-    public void constructor_followNull_True() {}
+@PrepareForTest({DungeonCamera.class, OrthographicCamera.class})
+public class DungeonCameraTest extends TestCase {
+    DungeonCamera cam;
+    IEntity entity;
+    Vector3 pos;
+    Frustum frustum;
 
-    @Test
-    public void constructor_followObject_True() {}
+    public void setUp() {
+        entity = Mockito.mock(IEntity.class);
+        pos = Mockito.mock(Vector3.class);
+        frustum = Mockito.mock(Frustum.class);
+        Mockito.when(entity.getPosition()).thenReturn(new Point(1, 1));
+        // evil parent:
+        PowerMockito.suppress(
+                MemberMatcher.constructor(OrthographicCamera.class, float.class, float.class));
+        PowerMockito.suppress(MemberMatcher.method(OrthographicCamera.class, "update"));
+        cam = PowerMockito.spy(new DungeonCamera(entity, 10, 10));
+        // set some thins, that are indispensable:
+        Whitebox.setInternalState(cam, "position", pos);
+        Whitebox.setInternalState(cam, "frustum", frustum);
+    }
 
-    @Test
-    public void update_followNull_True() {}
+    public void testUpdate() {
+        cam.update();
+        Mockito.verify(cam).update();
+        Mockito.verify(cam).getFollowedObject();
+        Mockito.verify(entity).getPosition();
+        Mockito.verify(cam.position).set(anyFloat(), anyFloat(), anyFloat());
+        Mockito.verifyNoMoreInteractions(cam, pos, frustum, entity);
+    }
 
-    @Test
-    public void update_followObject_True() {}
+    public void testFollow() {
+        IEntity e2 = Mockito.mock(IEntity.class);
+        cam.follow(e2);
+        assertEquals(e2, cam.getFollowedObject());
 
-    @Test
-    public void isPointInFrustum_inFrustum_True() {}
+        Mockito.verify(cam).follow(e2);
+        Mockito.verify(cam).getFollowedObject();
+        Mockito.verifyNoMoreInteractions(cam, pos, frustum, entity, e2);
+    }
 
-    @Test
-    public void isPointInFrustum_notInFrustum_False() {}
+    public void testGetFollowedObject() {
+        assertEquals(entity, cam.getFollowedObject());
+
+        Mockito.verify(cam).getFollowedObject();
+        Mockito.verifyNoMoreInteractions(cam, pos, frustum, entity);
+    }
+
+    public void testSetFocusPoint() {
+        cam.setFocusPoint(new Point(2, 2));
+        assertNull(cam.getFollowedObject());
+
+        Mockito.verify(cam).setFocusPoint(any());
+        Mockito.verify(cam).getFollowedObject();
+        Mockito.verifyNoMoreInteractions(cam, pos, frustum, entity);
+    }
+
+    public void testIsPointInFrustum() {
+        cam.isPointInFrustum(2, 2);
+
+        Mockito.verify(cam).isPointInFrustum(2, 2);
+        Mockito.verify(frustum).boundsInFrustum(any());
+        Mockito.verifyNoMoreInteractions(cam, pos, frustum, entity);
+    }
+
+    public void testGetFrustum() {
+        assertEquals(frustum, cam.getFrustum());
+
+        Mockito.verify(cam).getFrustum();
+        Mockito.verifyNoMoreInteractions(cam, pos, frustum, entity);
+    }
 }
