7de8cda19c493195a0535cb571bb14f02fe46a26
Tobias Grothe <85501570+Tobias321@users.noreply.github.com>
2021-12-16 13:07:09 +0100

    MainControllerTest.java test

--- code/core/test/controller/MainControllerTest.java
+++ code/core/test/controller/MainControllerTest.java
@@ -1,24 +1,71 @@
 package controller;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import graphic.DungeonCamera;
+import graphic.HUDCamera;
+import graphic.Painter;
+import level.LevelAPI;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(MainController.class)
+@PrepareForTest({MainController.class, Gdx.class})
 class MainControllerTest {
     MainController controller;
+    SpriteBatch batch;
 
     public MainControllerTest() {}
 
     @Before
-    public void init() {
+    public void init() throws Exception {
         controller = Mockito.spy(MainController.class);
+        batch = Mockito.mock(SpriteBatch.class);
+
+        Whitebox.setInternalState(Gdx.class, "gl", Mockito.mock(GL20.class));
+
+        PowerMockito.whenNew(EntityController.class)
+                .withNoArguments()
+                .thenReturn(Mockito.mock(EntityController.class));
+        PowerMockito.whenNew(Painter.class)
+                .withAnyArguments()
+                .thenReturn(Mockito.mock(Painter.class));
+        PowerMockito.whenNew(SpriteBatch.class)
+                .withAnyArguments()
+                .thenReturn(Mockito.mock(SpriteBatch.class));
+        PowerMockito.whenNew(HUDCamera.class)
+                .withNoArguments()
+                .thenReturn(Mockito.mock(HUDCamera.class));
+        PowerMockito.whenNew(HUDController.class)
+                .withAnyArguments()
+                .thenReturn(Mockito.mock(HUDController.class));
+        PowerMockito.whenNew(LevelAPI.class)
+                .withAnyArguments()
+                .thenReturn(Mockito.mock(LevelAPI.class));
+        PowerMockito.whenNew(DungeonCamera.class)
+                .withAnyArguments()
+                .thenReturn(Mockito.mock(DungeonCamera.class));
     }
 
     @Test
-    public void render() {}
+    public void render() {
+        controller.setSpriteBatch(batch);
+        Mockito.verify(controller).setSpriteBatch(batch);
+        Mockito.verifyNoMoreInteractions(controller, batch);
+
+        controller.render(10);
+
+        Mockito.verify(controller).render(10);
+        Mockito.verify(controller).setup();
+        Mockito.verify(controller, Mockito.times(2)).endFrame();
+
+        Mockito.verifyNoMoreInteractions(controller);
+    }
 }
