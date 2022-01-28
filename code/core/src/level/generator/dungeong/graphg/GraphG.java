6c0bf32ec7b15fc3bec413e43dedd17a82c6f05e
Andre Matutat <amatutat@web.de>
2022-01-28 11:00:29 +0100

    gjf

--- code/core/src/level/generator/dungeong/graphg/GraphG.java
+++ code/core/src/level/generator/dungeong/graphg/GraphG.java
@@ -19,7 +19,7 @@ import java.util.Random;
  * @author Andre Matutat
  */
 public class GraphG {
-    private final static int MAX_SOLUTIONS = 1000;
+    private static final int MAX_SOLUTIONS = 1000;
 
     /**
      * Read in a graph from .json with the given configuration.
