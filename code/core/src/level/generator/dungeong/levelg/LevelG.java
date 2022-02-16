7351cdee72c9923c9b6159e3d428e302d333ca3d
Tobias Grothe <85501570+Tobias321@users.noreply.github.com>
2022-02-16 14:45:29 +0100

    gradlew :spotlessApply ...

--- code/core/src/level/generator/dungeong/levelg/LevelG.java
+++ code/core/src/level/generator/dungeong/levelg/LevelG.java
@@ -2,6 +2,11 @@ package level.generator.dungeong.levelg;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 import level.elements.Level;
 import level.elements.graph.BFEdge;
 import level.elements.graph.Graph;
@@ -18,12 +23,6 @@ import level.tools.Coordinate;
 import level.tools.DesignLabel;
 import level.tools.LevelElement;
 import tools.Constants;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
 
 /**
  * Uses RoomG and GraphG to generate level.
