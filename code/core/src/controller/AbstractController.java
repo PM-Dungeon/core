8dc57d2e97ab7bd26a598e46f88ac663b3f388a7
Tobias Grothe <85501570+Tobias321@users.noreply.github.com>
2022-04-30 17:21:39 +0200

    Update AbstractController.java, change to ArrayDeque

--- code/core/src/controller/AbstractController.java
+++ code/core/src/controller/AbstractController.java
@@ -1,52 +1,53 @@
 package controller;
 
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
+import java.util.*;
 
 /**
  * A controller is a LinkedHashSet and manages elements of a specific type.
  *
  * @param <T> Type of elements to manage.
  */
-public abstract class AbstractController<T> extends LinkedHashSet<T> {
+public abstract class AbstractController<T> extends ArrayDeque<T> {
     /** Updates all elements that are registered at this controller */
     public abstract void update();
+
+    private final Set<T> set = new HashSet<>();
 
     @Override
     public boolean contains(Object o) {
         assert (o != null);
-        return super.contains(o);
+        return set.contains(o);
     }
 
-    /**
-     * Adds the element in front.
-     *
-     * <p>Elements that you add first with this method are drawn last, so they are drawn on top of
-     * the other elements. New elements added with this method are overdrawn with older ones.
-     *
-     * <p>Duplicates are not replaced or inserted, this is how HashSet normally behaves.
-     *
-     * @param t The element to be added in front.
-     * @return true, if success.
-     */
     @Override
-    public boolean add(T t) {
-        assert (t != null);
+    public void addFirst(T t) {
         if (contains(t)) {
-            return false;
+            return;
         }
-        ArrayList<T> list = new ArrayList<>(this);
-        clear();
-        boolean success = super.add(t);
-        for (T e : list) {
-            super.add(e);
-        }
-        return success;
+        set.add(t);
+        super.addFirst(t);
     }
 
     @Override
-    public boolean remove(Object o) {
-        assert (o != null);
-        return super.remove(o);
+    public void addLast(T t) {
+        if (contains(t)) {
+            return;
+        }
+        set.add(t);
+        super.addLast(t);
+    }
+
+    @Override
+    public T removeFirst() {
+        T e = super.removeFirst();
+        set.remove(e);
+        return e;
+    }
+
+    @Override
+    public T removeLast() {
+        T e = super.removeLast();
+        set.remove(e);
+        return e;
     }
 }
