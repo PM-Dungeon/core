b7da2d8c9af74ce7ce267cd6393b8e50c67f163d
Tobias Grothe <85501570+TGrothe@users.noreply.github.com>
2022-05-04 22:44:39 +0200

    Update AbstractController.java, add comments

--- code/core/src/controller/AbstractController.java
+++ code/core/src/controller/AbstractController.java
@@ -12,55 +12,75 @@ public abstract class AbstractController<T> implements Iterable<T> {
     public abstract void update();
 
     private final Map<ControllerLayer, List<T>> map = new TreeMap<>();
+    private final Map<T, List<T>> map2 = new HashMap<>();
 
+    /**
+     * Returns true, if the element is in this controller.
+     *
+     * @param t Element to look for.
+     * @return true, if the element is in this controller.
+     */
     public boolean contains(T t) {
         assert (t != null);
-        for (List<T> l : map.values()) {
-            if (l.contains(t)) {
-                return true;
-            }
-        }
-        return false;
+        return map2.containsKey(t);
     }
 
+    /**
+     * Adds the element with default layer (20) to this controller, if it is not already added.
+     *
+     * @param t Element to add.
+     * @return true, if this was successful.
+     */
     public boolean add(T t) {
         assert (t != null);
         return add(t, ControllerLayer.DEFAULT);
     }
 
+    /**
+     * Adds the element with the specific layer to this controller, if it is not already added.
+     *
+     * @param t Element to add.
+     * @param layer Layer to add the element in.
+     * @return true, if this was successful.
+     */
     public boolean add(T t, ControllerLayer layer) {
         assert (t != null);
         if (contains(t)) {
             return false;
         }
-        return map.computeIfAbsent(layer, x -> new ArrayList<>()).add(t);
+        map.computeIfAbsent(layer, x -> new ArrayList<>()).add(t);
+        map2.put(t, map.get(layer));
+        return true;
     }
 
+    /**
+     * Removes the element from this controller, if it is in this controller.
+     *
+     * @param t Element to remove.
+     * @return true, if this was successful.
+     */
     public boolean remove(T t) {
         assert (t != null);
-        boolean success = false;
-        for (List<T> l : map.values()) {
-            success |= l.remove(t);
+        if (!contains(t)) {
+            return false;
         }
-        return success;
-    }
-
-    public boolean remove(T t, ControllerLayer layer) {
-        assert (t != null);
-        return map.computeIfAbsent(layer, x -> new ArrayList<>()).remove(t);
-    }
-
-    public boolean isEmpty() {
-        for (List<T> l : map.values()) {
-            if (!l.isEmpty()) {
-                return false;
-            }
-        }
+        map2.get(t).remove(t);
+        map2.remove(t);
         return true;
+    }
+
+    /**
+     * Tests, if this controller has no elements.
+     *
+     * @return true, if empty.
+     */
+    public boolean isEmpty() {
+        return map2.isEmpty();
     }
 
     @Override
     public Iterator<T> iterator() {
+        // creates a list copy of merged lists
         List<T> list = new ArrayList<>();
         for (List<T> l : map.values()) {
             list.addAll(l);
