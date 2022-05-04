f4f1b1391254e53df3e30a17cdd66455a1a0dedf
Tobias Grothe <85501570+Tobias321@users.noreply.github.com>
2022-05-04 14:23:48 +0200

    Update EntityControllerTest.java, add test_update_withDifferentLayerAndDuplicates()

--- code/core/test/controller/EntityControllerTest.java
+++ code/core/test/controller/EntityControllerTest.java
@@ -2,6 +2,7 @@ package controller;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeFalse;
 import static org.junit.Assume.assumeTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -133,6 +134,36 @@ public class EntityControllerTest {
         Mockito.verifyNoMoreInteractions(entity1, entity2);
         assertTrue(controller.contains(entity1));
         assertTrue(controller.contains(entity2));
+        assertFalse(controller.isEmpty());
+    }
+
+    @Test
+    public void test_update_withDifferentLayerAndDuplicates() {
+        // should not be removed
+        when(entity1.removable()).thenReturn(false);
+        when(entity2.removable()).thenReturn(false);
+        assumeTrue(controller.add(entity1, ControllerLayer.TOP));
+        assumeTrue(controller.add(entity2, ControllerLayer.BOTTOM));
+        assumeFalse(controller.add(entity1, ControllerLayer.BOTTOM));
+        assumeFalse(controller.add(entity2, ControllerLayer.TOP));
+        assumeTrue(controller.remove(entity1));
+        assumeTrue(controller.add(entity1, ControllerLayer.TOP));
+
+        controller.update();
+
+        when(entity2.removable()).thenReturn(true);
+        controller.update();
+
+        verify(entity1, times(2)).removable();
+        verify(entity1, times(2)).update();
+        verify(entity1, times(2)).draw();
+        verify(entity2, times(2)).removable();
+        verify(entity2, times(1)).update();
+        verify(entity2, times(1)).draw();
+        Mockito.verifyNoMoreInteractions(entity1, entity2);
+
+        assertTrue(controller.contains(entity1));
+        assertFalse(controller.contains(entity2));
         assertFalse(controller.isEmpty());
     }
 }
