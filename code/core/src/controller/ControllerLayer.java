171afce6463c3c80de8e4e22e6a62ddd93f7c180
Tobias Grothe <85501570+TGrothe@users.noreply.github.com>
2022-05-11 19:45:45 +0200

    Remove DefaultLayer.java

--- code/core/src/controller/ControllerLayer.java
+++ code/core/src/controller/ControllerLayer.java
@@ -3,27 +3,21 @@ package controller;
 /**
  * A class to represent a layer on wich elements are drawn.
  *
- * <p>BOTTOM: 100 (you can de- or increase this value if you need to)<br>
- * DEFAULT: 200 (you can de- or increase this value if you need to)<br>
+ * <p>BOTTOM: 100 (you can de- or increase this value if you need to) <br>
+ * DEFAULT: 200 (you can de- or increase this value if you need to) <br>
  * TOP: 300 (you can de- or increase this value if you need to)
  *
  * <p>ControllerLayers with a higher value are drawn last (i.e. over other entities).
  */
 public class ControllerLayer implements Comparable<ControllerLayer> {
-    public static final ControllerLayer BOTTOM = new ControllerLayer(DefaultLayer.BOTTOM);
-    public static final ControllerLayer DEFAULT = new ControllerLayer(DefaultLayer.DEFAULT);
-    public static final ControllerLayer TOP = new ControllerLayer(DefaultLayer.TOP);
+    public static final int BOTTOM_VALUE = 100;
+    public static final int DEFAULT_VALUE = 200;
+    public static final int TOP_VALUE = 300;
+    public static final ControllerLayer BOTTOM = new ControllerLayer(BOTTOM_VALUE);
+    public static final ControllerLayer DEFAULT = new ControllerLayer(DEFAULT_VALUE);
+    public static final ControllerLayer TOP = new ControllerLayer(TOP_VALUE);
 
     public final int value;
-
-    /**
-     * Constructs a new ControllerLayer from a DefaultLayer.
-     *
-     * @param layer the DefaultLayer.
-     */
-    public ControllerLayer(DefaultLayer layer) {
-        this(layer.value);
-    }
 
     /**
      * Constructs a user-defined ControllerLayer. The value must be between 1 and 1000.
