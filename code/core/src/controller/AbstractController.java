dcbcfed19e28e8037bf25742fe294c200bca481f
Tobias Grothe <85501570+TGrothe@users.noreply.github.com>
2022-05-17 13:09:50 +0200

    rebase/merge

--- code/core/src/controller/AbstractController.java
+++ code/core/src/controller/AbstractController.java
@@ -7,12 +7,15 @@ import java.util.function.Consumer;
 /**
  * A controller manages elements of a certain type and is based on a layer system.
  *
+ * <p>Layer system means: All elements are listed in ascending order according to the layers in this
+ * controller and in the order they are arranged in the layer.
+ *
  * @param <T> Type of elements to manage.
  */
 public abstract class AbstractController<T extends DungeonElement> implements Iterable<T> {
 
-    private final Map<ControllerLayer, List<T>> map = new TreeMap<>();
-    private final Map<T, List<T>> map2 = new HashMap<>();
+    private final Map<ControllerLayer, List<T>> layerTreeMap = new TreeMap<>();
+    private final Map<T, List<T>> elementHashMap = new HashMap<>();
 
     /**
      * Updates all elements that are registered at this controller, removes deletable elements and
@@ -37,7 +40,7 @@ public abstract class AbstractController<T extends DungeonElement> implements It
      */
     public boolean contains(Object t) {
         assert (t != null);
-        return map2.containsKey(t);
+        return elementHashMap.containsKey(t);
     }
 
     /**
@@ -48,7 +51,7 @@ public abstract class AbstractController<T extends DungeonElement> implements It
      */
     public boolean add(T t) {
         assert (t != null);
-        return add(t, ControllerLayer.DEFAULT);
+        return addToLayer(t, ControllerLayer.DEFAULT);
     }
 
     /**
@@ -58,14 +61,45 @@ public abstract class AbstractController<T extends DungeonElement> implements It
      * @param layer Layer to add the element in.
      * @return true, if this was successful.
      */
-    public boolean add(T t, ControllerLayer layer) {
+    public boolean addToLayer(T t, ControllerLayer layer) {
         assert (t != null);
         if (contains(t)) {
             return false;
         }
-        map.computeIfAbsent(layer, x -> new ArrayList<>()).add(t);
-        map2.put(t, map.get(layer));
+        layerTreeMap.computeIfAbsent(layer, x -> new ArrayList<>()).add(t);
+        elementHashMap.put(t, layerTreeMap.get(layer));
         return true;
+    }
+
+    /**
+     * Adds all elements to the default controller, if they are not already added.
+     *
+     * @param c Collection to add.
+     * @return true, if this was modified.
+     */
+    public boolean addAll(Collection<T> c) {
+        assert (c != null);
+        boolean modified = false;
+        for (T e : c) {
+            modified |= addToLayer(e, ControllerLayer.DEFAULT);
+        }
+        return modified;
+    }
+
+    /**
+     * Adds all elements with the specific layer to this controller, if they are not already added.
+     *
+     * @param c Collection to add.
+     * @param layer Layer to add the element in.
+     * @return true, if this was modified.
+     */
+    public boolean addAllToLayer(Collection<T> c, ControllerLayer layer) {
+        assert (c != null);
+        boolean modified = false;
+        for (T e : c) {
+            modified |= addToLayer(e, layer);
+        }
+        return modified;
     }
 
     /**
@@ -79,8 +113,8 @@ public abstract class AbstractController<T extends DungeonElement> implements It
         if (!contains(t)) {
             return false;
         }
-        map2.get(t).remove(t);
-        map2.remove(t);
+        elementHashMap.get(t).remove(t);
+        elementHashMap.remove(t);
         return true;
     }
 
@@ -90,43 +124,68 @@ public abstract class AbstractController<T extends DungeonElement> implements It
      * @return true, if empty.
      */
     public boolean isEmpty() {
-        return map2.isEmpty();
+        return elementHashMap.isEmpty();
     }
 
     /** Clears the entire controller (removes all elements). */
     public void clear() {
-        map.clear();
-        map2.clear();
+        layerTreeMap.clear();
+        elementHashMap.clear();
     }
 
     /** Removes all elements in the specific layer. */
     public void clearLayer(ControllerLayer layer) {
-        for (T t : map.get(layer)) {
-            map2.remove(t);
+        for (T t : layerTreeMap.get(layer)) {
+            elementHashMap.remove(t);
         }
-        map.get(layer).clear();
+        layerTreeMap.get(layer).clear();
     }
 
     /**
-     * @return An unmodifiable iterator.
+     * @return An ordered list of all elements in this controller.
+     */
+    private List<T> toList() {
+        final List<T> list = new ArrayList<>();
+        for (List<T> l : layerTreeMap.values()) {
+            list.addAll(l);
+        }
+        return list;
+    }
+
+    /**
+     * @return An iterator.
      */
     @Override
     public Iterator<T> iterator() {
-        // creates a list copy of merged lists
-        List<T> list = new ArrayList<>();
-        for (List<T> l : map.values()) {
-            list.addAll(l);
-        }
-        return list.iterator();
-        // will not work with PowerMock:
-        // return map.values().stream().flatMap(List::stream).toList().iterator();
+        final List<T> list = toList();
+        final Iterator<T> it = list.iterator();
+        return new Iterator<T>() {
+            int i = 0;
+
+            @Override
+            public boolean hasNext() {
+                return it.hasNext();
+            }
+
+            @Override
+            public T next() {
+                i++;
+                return it.next();
+            }
+
+            @Override
+            public void remove() {
+                i--;
+                AbstractController.this.remove(list.get(i));
+                it.remove();
+            }
+        };
     }
 
     public int size() {
-        return map2.size();
+        return elementHashMap.size();
     }
 
-    // from Iterable
     @Override
     public void forEach(Consumer<? super T> action) {
         iterator().forEachRemaining(action);
@@ -134,6 +193,6 @@ public abstract class AbstractController<T extends DungeonElement> implements It
 
     @Override
     public String toString() {
-        return map2.keySet().toString();
+        return toList().toString();
     }
 }
