84724e5f6c96807dac2ce22a4e7671d2297506a7
Andre Matutat <andre.matutat@fh-bielefelde.de>
2022-05-24 00:18:11 +0200

    add tests for randomwalkgenerator

--- code/core/test/level/generator/RandomWalkGeneratorTest.java
+++ code/core/test/level/generator/RandomWalkGeneratorTest.java
@@ -1,3 +1,30 @@
 package level.generator;
 
-public class RandomWalkGeneratorTest {}
+import static org.junit.Assert.assertNotNull;
+
+import level.elements.Level;
+import level.generator.randomwalk.RandomWalkGenerator;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RandomWalkGeneratorTest {
+
+    private RandomWalkGenerator generator;
+    private Level level;
+
+    @Before
+    public void setup() {
+        generator = new RandomWalkGenerator();
+        level = generator.getLevel();
+    }
+
+    @Test
+    public void test_getLevel() {
+        assertNotNull(level);
+        assertNotNull(level.getEndTile());
+        assertNotNull(level.getStartTile());
+        // if the path is bigger than 0 it means, there is a path form start to end, so the level
+        // can be beaten.
+        assert ((level.findPath(level.getStartTile(), level.getEndTile()).getCount() > 0));
+    }
+}
