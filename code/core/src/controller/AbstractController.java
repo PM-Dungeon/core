0559f2b91f26d43921c3a8d92817cd60304bf4eb
Tobias Grothe <85501570+TGrothe@users.noreply.github.com>
2022-06-26 16:57:37 +0200

    Remove: AbstractDungeonElementController.java

--- code/core/src/controller/AbstractController.java
+++ code/core/src/controller/AbstractController.java
@@ -4,6 +4,14 @@ import basiselements.RemovableElement;
 import java.util.*;
 import java.util.function.Consumer;
 
+/**
+ * A controller manages elements of a certain type and is based on a layer system.
+ *
+ * <p>Layer system means: All elements are listed in ascending order according to the layers in this
+ * controller and in the order they are arranged in the layer.
+ *
+ * @param <T> Type of elements to manage.
+ */
 public abstract class AbstractController<T extends RemovableElement> implements Iterable<T> {
     private final Map<ControllerLayer, List<T>> layerTreeMap = new TreeMap<>();
     private final Map<T, List<T>> elementHashMap = new HashMap<>();
--- code/core/src/controller/EntityController.java
+++ code/core/src/controller/EntityController.java
@@ -3,4 +3,14 @@ package controller;
 import basiselements.Entity;
 
 /** Keeps a set of entities and calls their update method every frame. */
-public class EntityController extends AbstractDungeonElementController<Entity> {}
+public class EntityController extends AbstractController<Entity> {
+    /**
+     * Updates all elements that are registered at this controller, removes deletable elements and
+     * calls the update and draw method for every registered element.
+     */
+    @Override
+    public void process(Entity e) {
+        e.update();
+        e.draw();
+    }
+}
