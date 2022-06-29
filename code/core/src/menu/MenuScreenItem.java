917d664b512699cbe0c3e894ed4612e198ec0c7a
Tobias Grothe <85501570+TGrothe@users.noreply.github.com>
2022-06-29 20:28:26 +0200

    MenuScreenItem.java javadoc

--- code/core/src/menu/MenuScreenItem.java
+++ code/core/src/menu/MenuScreenItem.java
@@ -7,6 +7,12 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 public class MenuScreenItem {
     private final TextButton button;
 
+    /**
+     * Constructs a new MenuScreenItem as sub menu of a menu button.
+     *
+     * @param text the text of this item.
+     * @param listener the mouse click listener for this item.
+     */
     public MenuScreenItem(String text, TextButtonListener listener) {
         TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
         style.font = new BitmapFont();
@@ -17,10 +23,18 @@ public class MenuScreenItem {
         setFontSize(MenuScreenEntry.defaultScaleXY);
     }
 
+    /**
+     * @return the TextButton.
+     */
     public TextButton getButton() {
         return button;
     }
 
+    /**
+     * Sets the font size of this button.
+     *
+     * @param scaleXY the scale factor (1 = no rescaling).
+     */
     public void setFontSize(float scaleXY) {
         TextButton.TextButtonStyle style = button.getStyle();
         style.font.getData().setScale(scaleXY);
