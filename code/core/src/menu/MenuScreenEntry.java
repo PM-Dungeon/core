de20616b2d5a35931478f95bf6112487d7e03373
Tobias Grothe <85501570+TGrothe@users.noreply.github.com>
2022-06-29 20:24:14 +0200

    MenuScreenEntry.java javadoc

--- code/core/src/menu/MenuScreenEntry.java
+++ code/core/src/menu/MenuScreenEntry.java
@@ -13,6 +13,11 @@ public class MenuScreenEntry {
     private final List<MenuScreenItem> entryButtons = new ArrayList<>();
     private final VerticalGroup vg = new VerticalGroup();
 
+    /**
+     * Constructs a new MenuScreenEntry resp. a new TextButton.
+     *
+     * @param menuText the TextButton text.
+     */
     public MenuScreenEntry(String menuText) {
         TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
         style.font = new BitmapFont();
@@ -22,18 +27,30 @@ public class MenuScreenEntry {
         setFontSize(defaultScaleXY);
     }
 
+    /**
+     * Adds a MenuScreenItem as sub menu to this menu button.
+     *
+     * @param entry to add.
+     */
     public void add(MenuScreenItem entry) {
         entryButtons.add(entry);
     }
 
+    /**
+     * Removes the given MenuScreenItem from this menu button.
+     *
+     * @param entry to remove.
+     */
     public void remove(MenuScreenItem entry) {
         entryButtons.remove(entry);
     }
 
+    /** Removes all MenuScreenItems from this menu button. */
     public void clear() {
         entryButtons.clear();
     }
 
+    /** When mouse is over this menu button. */
     public void showVg() {
         vg.clear();
         for (MenuScreenItem e : entryButtons) {
@@ -41,6 +58,7 @@ public class MenuScreenEntry {
         }
     }
 
+    /** When mouse is no longer over this menu button. */
     public void hideVg() {
         for (MenuScreenItem e : entryButtons) {
             e.getButton().setChecked(false);
@@ -48,20 +66,34 @@ public class MenuScreenEntry {
         vg.clear();
     }
 
+    /**
+     * Sets the font size of this menu button.
+     *
+     * @param scaleXY the scale factor (1 = no rescaling).
+     */
     public void setFontSize(float scaleXY) {
         TextButton.TextButtonStyle style = menuButton.getStyle();
         style.font.getData().setScale(scaleXY);
         menuButton.setStyle(style);
     }
 
+    /**
+     * @return the menu button.
+     */
     public TextButton getMenuButton() {
         return menuButton;
     }
 
+    /**
+     * @return a list with all menu screen item of this menu button.
+     */
     public List<MenuScreenItem> getEntryButtons() {
         return entryButtons;
     }
 
+    /**
+     * @return the VerticalGroup of this menu button.
+     */
     public VerticalGroup getVg() {
         return vg;
     }
