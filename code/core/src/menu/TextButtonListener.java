c0918cf9e02c3363fe32078bbbdc04c50c8e1318
Tobias Grothe <85501570+TGrothe@users.noreply.github.com>
2022-06-29 20:32:17 +0200

    TextButtonListener.java javadoc

--- code/core/src/menu/TextButtonListener.java
+++ code/core/src/menu/TextButtonListener.java
@@ -4,6 +4,10 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
+/**
+ * This abstract class is a ClickListener that must be implemented for all screen items. You only
+ * need to implement the clicked event method.
+ */
 public abstract class TextButtonListener extends ClickListener {
     public void setButton(TextButton button) {
         button.addListener(this);
