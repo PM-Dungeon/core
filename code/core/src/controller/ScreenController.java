2c457d11fabab6df7c1db0fbd190566446807c99
AHeinisch <aheinisch@fh-bielefeld.de>
2022-07-25 14:26:14 +0200

    fix layersystem for ScreenController

--- code/core/src/controller/ScreenController.java
+++ code/core/src/controller/ScreenController.java
@@ -6,11 +6,17 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.viewport.ScalingViewport;
+import java.util.Iterator;
 import tools.Constants;
 
 public class ScreenController extends AbstractController<ScreenElement> {
     protected Stage stage;
 
+    /**
+     * Creates a Screencontroller with a ScalingViewport which stretches the ScreenElements on resize
+     *
+     * @param batch the batch which should be used to draw with
+     */
     public ScreenController(SpriteBatch batch) {
         super(batch);
         stage =
@@ -34,13 +40,24 @@ public class ScreenController extends AbstractController<ScreenElement> {
     public void process(ScreenElement e) {}
 
     @Override
-    public boolean add(ScreenElement screenElement) {
-        boolean add = super.add(screenElement);
+    public boolean add(ScreenElement screenElement, ControllerLayer layer) {
+        boolean add = super.add(screenElement, layer);
         if (!add) {
             return false;
         }
         stage.addActor(screenElement.getActor());
+        reorder();
+
         return add;
+    }
+
+    /** reorders The Elements for the Stage */
+    private void reorder() {
+        Iterator<ScreenElement> it = this.iterator();
+        int index = 0;
+        while (it.hasNext()) {
+            it.next().getActor().setZIndex(index++);
+        }
     }
 
     @Override
